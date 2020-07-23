1b39ffdf1f719e2d475b9d1dff5962c0330dbe06
Jenjo <longinuspun@gmail.com>
2020-07-23 14:45:24 -0700

    Use fab in fragment

--- app/src/main/java/com/me/gasapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/me/gasapp/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.me.gasapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.me.gasapp.R
 
 class HomeFragment : Fragment() {
@@ -26,6 +29,17 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val fab: FloatingActionButton = root.findViewById(R.id.fab)
+        fab.setOnClickListener{ view ->
+            Snackbar.make(view, "Button Pressed", Snackbar.LENGTH_LONG).show()
+            Log.d("FAB","Pressed")
+//                view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+        }
+
+
         return root
     }
 }
