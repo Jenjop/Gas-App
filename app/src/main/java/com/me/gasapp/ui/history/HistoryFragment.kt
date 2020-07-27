3ea7dbc86cd8d286def0b23acba651b42b695fc5
Jenjo <longinuspun@gmail.com>
2020-07-27 14:09:07 -0700

    Revert "Cursor adapter"
    This reverts commit af49ae13ee0d016f121a243f43603343f853aef9.
    Convert sqlite to mutablelist instead of creating cursor adapter for recyclerview

--- app/src/main/java/com/me/gasapp/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/me/gasapp/ui/history/HistoryFragment.kt
@@ -1,24 +1,19 @@
 package com.me.gasapp.ui.history
 
-import android.R
-import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SimpleCursorAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.me.gasapp.DBManager
 import com.me.gasapp.DataList
-import com.me.gasapp.DatabaseHelper
+import com.me.gasapp.R
 import com.me.gasapp.SharedViewModel
-
 
 class HistoryFragment : Fragment() {
 
@@ -26,18 +21,6 @@ class HistoryFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var model: SharedViewModel
     private var dataEntries: DataList = mutableListOf()
-
-
-//    //DB
-    private lateinit var dbManager: DBManager
-    private lateinit var adapter: SimpleCursorAdapter
-    val from = arrayOf(
-        DatabaseHelper._ID,
-        DatabaseHelper.DATE,
-        DatabaseHelper.DIST,
-        DatabaseHelper.GAS
-    )
-    val to = intArrayOf(R.id.id, R.id.title, R.id.desc)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -59,12 +42,6 @@ class HistoryFragment : Fragment() {
             copyDataEntries(it)
             Log.d("SVM", "Observed Change")
         })
-
-        //DB
-        dbManager = DBManager(context!!);
-        dbManager.open()
-        val cursor: Cursor? = dbManager.fetch();
-        adapter = SimpleCursorAdapter(context, R.layout.recycler_item_layout, cursor, from, to, 0)
 
 
 //        val data: MutableList<DoubleArray> = root.getData
