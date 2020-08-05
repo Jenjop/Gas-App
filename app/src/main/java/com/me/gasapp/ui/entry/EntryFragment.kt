fd7830c5c876a816251d635033b883000b7e73ab
Jenjo <longinuspun@gmail.com>
2020-08-04 17:29:40 -0700

    Default current date

--- app/src/main/java/com/me/gasapp/ui/entry/EntryFragment.kt
+++ app/src/main/java/com/me/gasapp/ui/entry/EntryFragment.kt
@@ -1,6 +1,7 @@
 package com.me.gasapp.ui.entry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,9 +18,9 @@ class EntryFragment : Fragment() {
     private var today: Long = 0
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_entry, container, false)
         //https://hackernoon.com/how-to-use-new-material-date-picker-for-android-s7k32w0
@@ -30,11 +31,14 @@ class EntryFragment : Fragment() {
 
         //https://github.com/material-components/material-components-android/blob/master/catalog/java/io/material/catalog/datepicker/DatePickerMainDemoFragment.java
         val dateButton: Button = root.findViewById(R.id.input_date)
-        dateButton.setOnClickListener {view->
-//            today = MaterialDatePicker.todayInUtcMilliseconds()
+        dateButton.setOnClickListener { view ->
+            today = MaterialDatePicker.todayInUtcMilliseconds()
             val builder = MaterialDatePicker.Builder.datePicker()
+            builder.setSelection(today)
             val picker = builder.build()
             picker.show(childFragmentManager, picker.toString())
+            
+
         }
 
         return root
